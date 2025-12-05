7d29027ccf494b247258b09f74992036a7b81dbd
MauroJuliano <maurodeveloper@outlook.com>
2025-12-04 23:15:02 -0300

    Add hero button action, and languages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 import { loadSection } from './loadSections.js';
 import { initItensAnimationScroll } from "./scrollAnimations.js";
 import { initCarousel } from './carousel.js';
+import { initLanguageSelector } from './language.js';
+import { initSocialLinks } from './socialLinks.js';
 
-
-
+// HERO
 loadSection("hero", "sections/hero.html", () => {
+  
+  // scrollBtn só existe depois que hero.html foi carregado!
   const btn = document.getElementById('scrollBtn');
   if (btn) {
     btn.addEventListener('click', () => {
@@ -13,18 +16,28 @@ loadSection("hero", "sections/hero.html", () => {
       });
     });
   }
+
+  // Aqui está PERFEITO
+   initLanguageSelector({
+        jsonPath: "./data/", // pasta onde ficam os JSON
+        defaultLang: "en",
+        elementsToUpdate: {
+            "hero-name": "name",
+            "hero-title": "title",
+            "scrollBtn": "cta"
+        }
+    });
+
+    initSocialLinks();
 });
 
-
+// CAREER
 loadSection("career", "sections/career.html", initItensAnimationScroll);
+
+// PROJECTS
 loadSection("projects", "sections/projects.html", () => {
-    initCarousel();
-  });
+  initCarousel();
+});
+
+// CONTACT
 loadSection("contact", "sections/contact.html");
-
-
-  document.getElementById('scrollBtn').addEventListener('click', () => {
-    document.getElementById('contact').scrollIntoView({
-      behavior: "smooth"
-    });
-  });
